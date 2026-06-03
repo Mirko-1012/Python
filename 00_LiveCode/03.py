7723fd1c3ffc905b5ad17c9e4bd97cb8bb8fe475
Mirko-1012 <di.natale.mirko.9@gmail.com>
2026-06-03 12:42:53 +0200

    4

---
+++ 00_LiveCode/03.py
@@ -0,0 +1,15 @@
+# Partiamo da un dizionario
+a = {"  Mela ": 10, "b": 20, "  MELA  ": 30, "c": 40}
+
+b = {}
+
+for chiave, valore in a.items():
+    stringa_pulita = chiave.strip().lower()
+
+    if stringa_pulita != "":
+        b[stringa_pulita] = valore
+
+lunghezza = len(b)
+
+print(b)
+print(lunghezza)
